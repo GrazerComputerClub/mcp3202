291b88f38926537ea1886ca4e407eb4baa4ab439
m.stroh76 <m.stroh76@gmail.com>
2019-01-31 21:13:07 +0100

    Start/Stop logging

--- mcp3202.cpp
+++ mcp3202.cpp
@@ -60,6 +60,17 @@ static int AnalogRead(int spiChannel, int analogChannel) {
 }
 
 
+void StoreMsg(const char* szMesg) {
+    FILE* logile = fopen("voltage.log","a");
+    if (logile) {
+        fprintf(logile, "%s\n", szMesg);
+        fclose(logile);
+    } else {
+        fprintf(stderr, "Can't open log file: %s\n", strerror(errno));
+    }
+}
+
+
 void StoreVoltages(time_t rawtime, float elepsed, int CH0, int CH1) {
     char timestr[80];
     struct tm * timeinfo;
@@ -69,7 +80,7 @@ void StoreVoltages(time_t rawtime, float elepsed, int CH0, int CH1) {
 
     timeinfo = localtime(&rawtime);
     strftime(timestr, 80, "%d.%m.%Y %H:%M:%S", timeinfo);
-    printf("MCP3202(CE1): '%s' CH 0,1 = %04d, %04d -> %.3f, %.3f V\n", timestr,CH0, CH1, value0, value1);
+    printf("MCP3202(CE1): '%s' CH 0,1 = %04d, %04d -> %.3f, %.3f V\n", timestr, CH0, CH1, value0, value1);
     FILE* logile = fopen("voltage.log","a");
     if (logile) {
         fprintf(logile,"%s\t%.0f\t%.3f\t%.3f\n", timestr, elepsed, value0, value1);
@@ -87,6 +98,8 @@ int main(int argc, char *argv []){
     time_t rawtime;
     time_t timerref;
     struct tm y2k19 = {0};
+    int nLoopCount = 0;
+    int nSleepFactor = 1;
 
     memset(&sa, 0, sizeof(struct sigaction));
     sa.sa_handler = sighandler;
@@ -95,24 +108,34 @@ int main(int argc, char *argv []){
     sigaction(SIGTERM, &sa, NULL);
 
     y2k19.tm_hour = 0;   y2k19.tm_min = 0; y2k19.tm_sec = 0;
-    y2k19.tm_year = 118; y2k19.tm_mon = 0; y2k19.tm_mday = 1;
+    y2k19.tm_year = 119; y2k19.tm_mon = 0; y2k19.tm_mday = 1;
     timerref = mktime(&y2k19);
 
-    static int hSPI;
     const int spiChannel = 1;
+    static int hSPI;
     int CH0, CH1;
 
     wiringPiSetup();
     spiSetup(spiChannel, hSPI);
+    StoreMsg("MCP3202 START");
     do {
+        nLoopCount++;
         CH0 = AnalogRead(spiChannel, 0);
         CH1 = AnalogRead(spiChannel, 1);
         time(&rawtime);
-        StoreVoltages(rawtime, difftime(rawtime, timerref), CH0, CH1);
-        sleep(10);
+        if (CH0>0 || CH1>0) {
+            StoreVoltages(rawtime, difftime(rawtime, timerref), CH0, CH1);
+            nSleepFactor = 1;
+        } else {
+            fprintf(stderr, "no answer\n");
+            if (nSleepFactor<10) {
+                nSleepFactor++;
+            }
+        }
+        sleep(60*nSleepFactor);
     } while(!end);
     close(hSPI);
-
+    StoreMsg("MCP3202 FINISHED");
     return 0;
 }
 
